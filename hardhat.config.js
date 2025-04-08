65bf219df9a9a4fd3ac71f7303b2f5f38c36e281
kmilo <kmilo.denis.glez@yandex.com>
2025-04-08 19:23:24 -0400

    add gasReporter configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,19 @@
 require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
 
 require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.28",
+  gasReporter: {
+    enabled: true, // Enable gas reporting
+    currency: "USD", // Optional: Convert gas to a specific currency
+    gasPrice: 20 // Optional: Gas price in Gwei
+  },
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-  },
+      url: "http://127.0.0.1:8545"
+    }
+  }
 };
